3d179c02567d99a22c973cf4c9393cd29eeb5546
romanpovzyk <roman.povzyk@gmail.com>
2020-05-21 21:51:49 +0300

    impove task #1

--- task_1.py
+++ task_1.py
@@ -1,16 +1,18 @@
 import random
 
-computer_number = random.randint(1, 10)
-user_number = input("Введіть число від 1 до 10\n")
+while True:
 
-if user_number.isdecimal():
-    if int(user_number) < 1 or int(user_number) > 10:
-        print("Ваше число не відповідає правилам!")
-    elif int(user_number) == int(computer_number):
-        print("Вітаємо, ви вгадали!")
+    user_number = input("Введіть число від 1 до 10\n")
+    comp_number = random.randint(1, 10)
+
+    if user_number.isdecimal():
+        if int(user_number) < 1 or int(user_number) > 10:
+            print("Ваше число не відповідає правилам!\n")
+        elif int(user_number) == comp_number:
+            print("Вітаємо, ви вгадали!\n")
+        else:
+            print(f"На жаль, ви не вгадали! Це число {comp_number}\n")
     else:
-        print(f"На жаль, ви не вгадали! Це число {computer_number}")
-else:
-    print("Ви ввели не число, тому не зіграєте")
+        print("Ви ввели не число, тому не зіграєте\n")
 
 
